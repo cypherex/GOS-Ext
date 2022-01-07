08347c49f26df01848ae8d4ac6e129458a8b12de
cypherex <43692292+cypherex@users.noreply.github.com>
2022-01-07 12:07:19 +0800

    tower range script

---
+++ TowerRanges.lua
@@ -0,0 +1,23 @@
+
+local function isTowerOnScreen()
+    for i = 1, GameTurretCount() do
+        local tower = GameTurret(i)
+        if i.pos2D.onScreen then
+            return true, tower
+        end
+    end
+    return false, nil
+end
+
+local function onDraw()
+    local onScreen, tower = isTowerOnScreen()
+    if onScreen then
+        Draw.Circle(tower.pos, 775 + tower.boundingRadius, Draw.Color(255, 255,0,0))
+    end
+end
+
+local function onLoad()
+    Callback.Add("Draw", function () onDraw() end )
+end
+
+Callback.Add("Load",onLoad)
