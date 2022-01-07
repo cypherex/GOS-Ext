c173edd65a4a1aea72c54aa3f861a3b49dde7576
cypherex <43692292+cypherex@users.noreply.github.com>
2022-01-07 12:27:09 +0800

    oops

--- TowerRanges.lua
+++ TowerRanges.lua
@@ -1,23 +1,21 @@
 
-local function isTowerOnScreen()
-    for i = 1, GameTurretCount() do
-        local tower = GameTurret(i)
-        if tower.pos2D.onScreen then
-            return true, tower
-        end
-    end
-    return false, nil
-end
-
+local menu
 local function onDraw()
-    local onScreen, tower = isTowerOnScreen()
-    if onScreen then
-        Draw.Circle(tower.pos, 775 + tower.boundingRadius, Draw.Color(255, 255,0,0))
+    if menu.Enabled:Value() then
+        for i = 1, Game.TurretCount() do
+            local tower = Game.Turret(i)
+            if tower.pos2D.onScreen then
+                Draw.Circle(tower.pos, 775 + tower.boundingRadius, Draw.Color(255, 255,0,0))
+            end
+        end
     end
 end
 
 local function onLoad()
     Callback.Add("Draw", function () onDraw() end )
+
+    menu = MenuElement({type = MENU, id = "tower_range", name = "TowerRangeScript"})
+    menu:MenuElement({id = "Enabled", name = "Enabled", value = true, toggle=true})
 end
 
 Callback.Add("Load",onLoad)
