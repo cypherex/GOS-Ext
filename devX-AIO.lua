a363e55198dbd6ed71c5fc5a52798ef354188afd
cypherex <43692292+cypherex@users.noreply.github.com>
2022-01-18 12:36:07 +0800

    added syndra

--- devX-AIO.lua
+++ devX-AIO.lua
@@ -1,5 +1,6 @@
 
-local Heroes = {"Swain","RekSai","Elise"}
+local Heroes = {"Swain","RekSai","Elise","Syndra"}
+
 if not table.contains(Heroes, myHero.charName) then return end
 
 require "DamageLib"
@@ -15,9 +16,15 @@ local GameTurret = Game.Turret
 local GameTurretCount = Game.TurretCount
 local GameHeroCount     = Game.HeroCount
 local GameHero          = Game.Hero
+local GameMinionCount     = Game.MinionCount
+local GameMinion          = Game.Minion
+
+local GameObjectCount     = Game.ObjectCount
+local GameObject       = Game.Object
 local TableInsert       = _G.table.insert
 
 local orbwalker         = _G.SDK.Orbwalker
+local HealthPrediction         = _G.SDK.HealthPrediction
 
 
 
@@ -43,7 +50,7 @@ local function getEnemyHeroes()
     local EnemyHeroes = {}
     for i = 1, Game.HeroCount() do
         local Hero = Game.Hero(i)
-        if Hero.isEnemy then
+        if Hero.isEnemy and not Hero.dead then
             table.insert(EnemyHeroes, Hero)
         end
     end
@@ -142,6 +149,27 @@ end
 local function getTowerDamage()
     local minutes = Game.Timer() / 60
     return 9 * math.floor(minutes - 1.5) + 170
+end
+
+
+
+local function ClosestPointOnLineSegment(p, p1, p2)
+    local px = p.x
+    local pz = p.z
+    local ax = p1.x
+    local az = p1.z
+    local bx = p2.x
+    local bz = p2.z
+    local bxax = bx - ax
+    local bzaz = bz - az
+    local t = ((px - ax) * bxax + (pz - az) * bzaz) / (bxax * bxax + bzaz * bzaz)
+    if (t < 0) then
+        return p1, false
+    end
+    if (t > 1) then
+        return p2, false
+    end
+    return {x = ax + t * bxax, z = az + t * bzaz}, true
 end
 
 --------------------------------------------------
@@ -458,6 +486,7 @@ class "RekSai"
             self.Menu.ELogic:MenuElement({TYPE = _G.SPACE, name = "Auto Rappel will be active if being attacked by a tower that can kill"})
             self.Menu.ELogic:MenuElement({id = "HP", name = "Percent HP", value = 25, min = 0, max = 100})
 
+        self.Menu:MenuElement({id = "AttackReset", name = "Use Attack Resets and Spacing", value = true, toggle = true})
         self.Menu:MenuElement({id = "Clear", name = "Lane / JG Clear - Use Abilities", value = true, toggle = true, key = string.byte("T")})
     end
 
@@ -471,9 +500,27 @@ class "RekSai"
     ------------
 
     function Elise:onPostAttack(args)
+        if not self.Menu.AttackReset:Value() then return end
+        if not self.Menu.Clear:Value() and (orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR] or orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR]) then
+            return
+        end
+
         local target = orbwalker:GetTarget();
-        if target and isSpellReady(_Q) and self.isInSpiderForm then
-            Control.CastSpell(HK_Q, target)
+        
+        if target then
+            if self.isInSpiderForm then
+                
+                if isSpellReady(_W) then
+                    Control.CastSpell(HK_W, target)
+                    orbwalker:__OnAutoAttackReset()
+                    return
+                end
+                
+                if isSpellReady(_Q) then
+                    Control.CastSpell(HK_Q, target)
+                end  
+            end
+            
         end
     end
 
@@ -536,26 +583,44 @@ class "RekSai"
                 if isSpellReady(_E) and myHero.pos:DistanceTo(target.pos) <= 1075 then
                     castSpell(self.eSpellData, HK_E, target)
                 end
-                if isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= 625 then
-                    Control.CastSpell(HK_Q, target)
+                if orbwalker:CanAttack(target) then
+                    Control.Attack(target)
                 end
+                if isSpellReady(_Q)  and myHero.pos:DistanceTo(target.pos) <= 750 then
+                    Control.CastSpell(HK_Q, target)
+                    print("Cast Qx")
+                end
+                
+                
+                
                 if isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= 950 then
                     Control.CastSpell(HK_W, target)
+                    print("Cast W")
                 end
                 if isSpellReady(_R) and not isSpellReady(_Q) and not isSpellReady(_W) then
                     Control.CastSpell(HK_R)
+                    if myHero.pos:DistanceTo(target.pos) >= 300 then
+                        Control.CastSpell(HK_Q)
+                        print("Casting Q2")
+                    end
                     self.isInSpiderForm = true
                 end
             end 
             if self.isInSpiderForm then
                 
-                if isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= 475 then
+                
+                if isSpellReady(_Q) then
                     Control.CastSpell(HK_Q, target)
+                    print("Casting Q")
+                end  
+                if not self.Menu.AttackReset:Value() then
+                    
+                    if isSpellReady(_W) then
+                        Control.CastSpell(HK_W, target)
+                        orbwalker:__OnAutoAttackReset()
+                    end
                 end
-                if isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= 700 then
-                    Control.CastSpell(HK_W, target)
-                end
-            end
+            end 
         end
     end
 
@@ -564,7 +629,7 @@ class "RekSai"
         local target = orbwalker:GetTarget()
         if target then
             if not self.isInSpiderForm then
-                if isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= 625 then
+                if isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= 475 then
                     Control.CastSpell(HK_Q, target)
                 end
                 if isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= 950 then
@@ -579,14 +644,332 @@ class "RekSai"
                 if isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= 475 then
                     Control.CastSpell(HK_Q, target)
                 end
-                if isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= 700 then
-                    Control.CastSpell(HK_W, target)
+                if not self.Menu.AttackReset:Value() then
+                    if isSpellReady(_W) then
+                        Control.CastSpell(HK_W, target)
+                    end
                 end
             end
         end
     end
 
+--------------------------------------------------
+-- Syndra
+--------------
+class "Syndra"
+        
+    function Syndra:__init()	     
+        print("devX-Syndra Loaded") 
+        self:LoadMenu()   
+        
+        self.qSpellData = {Type = GGPrediction.SPELLTYPE_CIRCLE, Delay = 0.65, Radius = 180, Range = 800, Speed = math.huge, Collision = false}
+        self.wSpellData = {Type = GGPrediction.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 200, Range = 925, Speed = 1450, Collision = false}
+
+        Callback.Add("Draw", function() self:Draw() end)           
+        Callback.Add("Tick", function() self:onTickEvent() end)    
+    end
+
+    --
+    -- Menu 
+    function Syndra:LoadMenu() --MainMenu
+        self.Menu = MenuElement({type = MENU, id = "devSyndra", name = "DevX Syndra v1.0"})
+        -- ComboMenu  
+
+        self.Menu:MenuElement({type = MENU, id = "BlockR", name = "Ultimate Blacklist"})
+            DelayAction(function()
+                for i, Hero in pairs(getEnemyHeroes()) do
+                    self.Menu.BlockR:MenuElement({id = Hero.charName, name = "Block Ult on: "..Hero.charName, value = false})		
+                end		
+            end,0.2)
+        
+        self.Menu:MenuElement({id = "Clear", name = "Q Clear", value = true, toggle = true, key = string.byte("T")})
+        self.Menu:MenuElement({id = "WHarrass", name = "W Harass", value = true, toggle = true, key = string.byte("G")})
+        self.Menu:MenuElement({id = "AntiMelee", name = "Anti-Melee Range", value = 365, min = 0, max = 500, step = 1})
+        self.Menu:MenuElement({id = "DrawAntiMelee", name = "Draw Anti-Melee Range", value = true, toggle = true})
+                
+    end
+
+    function Syndra:Draw()
+        
+        local hero2d = myHero.pos2D
+        if self.Menu.Clear:Value() then
+            Draw.Text("Lane Clear Enabled [T]", 15, hero2d.x - 30, hero2d.y + 30, Draw.Color(255, 0, 255, 0))
+        else
+            Draw.Text("Lane Clear Disabled [T]", 15, hero2d.x - 30, hero2d.y + 30, Draw.Color(255, 255, 0, 0))
+        end
+        
+        if self.Menu.WHarrass:Value() then
+            Draw.Text("W Harass Enabled [G]", 15, hero2d.x - 30, hero2d.y + 50, Draw.Color(255, 0, 255, 0))
+        else
+            Draw.Text("W Harass  Disabled [G]", 15, hero2d.x - 30, hero2d.y + 50, Draw.Color(255, 255, 0, 0))
+        end
+        if self.Menu.DrawAntiMelee:Value() then
+            
+            Draw.Circle(myHero.pos, self.Menu.AntiMelee:Value(), 1, Draw.Color(255, 0, 0, 10))
+        end
+    end
+
+
+    --------------------------------------------------
+    -- Callbacks
+    ------------
+    function Syndra:onTickEvent()
+
+        if isSpellReady(_Q) then
+            self:AutoQ()
+        end
+        self:AutoUlt()
+        self:AntiMelee()
+        if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] then
+            self:Combo()
+        end
+
+        if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_HARASS] then
+            self:Harass()
+        end
+
+        if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR] and self.Menu.Clear:Value() then
+            self:LaneClear()
+        end
+    end
+    ----------------------------------------------------
+    -- Combat Functions
+    ---------------------
+ 
+    function Syndra:castQE(target)
+        local pos = myHero.pos + (target.pos - myHero.pos):Normalized() * 690
+        self.isInEQ = true
+
+        Control.CastSpell(HK_E, target.pos)
+        orbwalker:SetMovement(false)
+        DelayAction(
+            function ()
+                Control.CastSpell(HK_Q, pos)
+                orbwalker:SetMovement(true)
+                self.isInEQ = false
+            end, 0.2
+        )
+    end
+
+    function Syndra:castEQ(target, distance)
+        local castPosition = myHero.pos + (target.pos - myHero.pos):Normalized() * math.min(distance*3/4, 750)
+        Control.CastSpell(HK_Q, castPosition) 
+        orbwalker:SetMovement(false)
+        self.isInEQ = true
+        DelayAction(function()
+            Control.CastSpell(HK_E, target.pos)
+            self.isInEQ = false
+            orbwalker:SetMovement(true)
+        end,0.12)	
+    end
+
+    function Syndra:castW(wObject, target)
+        Control.CastSpell(HK_W, wObject)
+        self.isInW = true
+        DelayAction(function()
+            castSpell(self.wSpellData, HK_W, target)
+            self.isInW = false
+        end
+        , 0.05)
+    end
+
+    ----------------------------------------------------
+    -- Other Functions
+    ---------------------
+
+    function Syndra:getWObject()
+        for i = 1, GameObjectCount() do
+            local gameObj = GameObject(i)
+            if gameObj and gameObj.name == "Seed" and not gameObj.dead then
+                local distance = myHero.pos:DistanceTo(gameObj.pos)
+                if distance < 900 then
+                    return gameObj
+                end
+            end
+        end 
+
+        for i = 1, GameMinionCount() do
+            local minion = GameMinion(i)
+            
+            if minion and minion.isEnemy and not minion.dead and minion.pos:To2D().onScreen  then
+                local distance = myHero.pos:DistanceTo(minion.pos)
+                if distance < 900 then
+                    return minion
+                end
+            end
+        end 
+        return nil
+    end
+
+    function Syndra:getRDamage(target)
+        return getdmg("R", target, myHero, 2) * myHero:GetSpellData(_R).ammo
+    end
+
+    function Syndra:getQDamage(target)
+        local damage = 0
+        
+        if myHero:GetSpellData(_Q).level > 0 and myHero:GetSpellData(_Q).level < 5 then
+            damage = getdmg("Q", target, myHero)
+        end
+        
+        if myHero:GetSpellData(_Q).level == 5 then
+            damage = getdmg("Q", target, myHero) + (getdmg("Q", target, myHero)*0.25)
+        end
+        return damage
+    end
     
+    function Syndra:getWDamage(target)
+        local damage = 0
+        
+        if myHero:GetSpellData(_W).level > 0 and myHero:GetSpellData(_W).level < 5 then
+            damage = getdmg("W", target, myHero)
+        end
+        
+        if myHero:GetSpellData(_W).level == 5 then
+            damage = getdmg("W", target, myHero) + (0.2 * (({70, 110, 150, 190, 230})[myHero:GetSpellData(_W).level] + 0.7 * myHero.ap))
+        end
+        return damage
+    end
+    
+    function Syndra:getBestClearMinion()
+        local bestCount = 0
+        local bestPosition = nil
+        for i = 1, GameMinionCount() do
+            local minion1 = GameMinion(i)
+            local count = 0
+            if minion1 and not minion1.dead and minion1.isEnemy and myHero.pos:DistanceTo(minion1.pos) < 800 then 
+                for j = 1, GameMinionCount() do
+                    local minion2 = GameMinion(j)
+                    if minion2 and not minion2.dead and minion2.isEnemy and minion1.pos:DistanceTo(minion2.pos) < self.qSpellData.Radius then
+                        count = count + 1
+                    end
+                end
+                if count > bestCount then
+                    bestCount = count
+                    bestPosition = minion1.pos
+                end
+            end
+        end
+        return bestPosition, bestCount
+    end
+    ----------------------------------------------------
+    -- Combat Modes
+    ---------------------
+    
+    function Syndra:AutoUlt()
+        if self.isInEQ or self.isInW or not isSpellReady(_R) then return end
+        for i, enemy in pairs(getEnemyHeroes()) do
+            if enemy and not enemy.dead and myHero.pos:DistanceTo(enemy.pos) <  725 then
+                local notBlacklisted = self.Menu.BlockR[enemy.charName] and not self.Menu.BlockR[enemy.charName]:Value()
+                local enemyCountClose = #getEnemyHeroesWithinDistance(1500)
+                if notBlacklisted or enemyCountClose == 1 then
+                    local damages = self:getRDamage(enemy) 
+                    if enemy.health <= damages then
+                        Control.CastSpell(HK_R, enemy)
+                    end
+                end
+            end
+        end
+    end
+
+    function Syndra:Combo()
+        local target = _G.SDK.TargetSelector:GetTarget(1000, _G.SDK.DAMAGE_TYPE_MAGICAL);
+        
+        if self.isInEQ or self.isInW then return end
+        if target then
+            
+            local distance = myHero.pos:DistanceTo(target.pos)
+            
+            if isSpellReady(_R) and distance < 700 then
+                local notBlacklisted = self.Menu.BlockR[target.charName] and not self.Menu.BlockR[target.charName]:Value()
+                local enemyCountClose = #getEnemyHeroesWithinDistance(1500)
+
+                if notBlacklisted or enemyCountClose == 1 then
+                    local QDmg = isSpellReady(_Q) and self:getQDamage(target) or 0
+                    local WDmg = isSpellReady(_W) and self:getWDamage(target) or 0
+                    local EDmg = isSpellReady(_E) and getdmg("E", target, myHero) or 0
+                    local damages = self:getRDamage(target) + QDmg + WDmg + EDmg
+                    if target.health <= damages then
+                        Control.CastSpell(HK_R, target)
+                    end
+                end
+            end
+
+            if isSpellReady(_E) then
+                if distance < 650 then
+                    self:castQE(target)
+                elseif distance < 1200 then
+                    self:castEQ(target, distance)
+                end
+            end
+            
+            if self.isInEQ then return end
+
+            if distance < 850 and isSpellReady(_Q) then
+                castSpell(self.qSpellData, HK_Q, target)
+            end
+
+            if isSpellReady(_W) and distance < 850 then
+                local wObject = self:getWObject()
+                if wObject then
+                    self:castW(wObject, target)
+                end
+            end
+
+        end
+    end
+
+    function Syndra:Harass()
+        local target = _G.SDK.TargetSelector:GetTarget(1000, _G.SDK.DAMAGE_TYPE_MAGICAL);
+        
+        if self.isInEQ or self.isInW then return end
+
+        if target then
+            
+            local distance = myHero.pos:DistanceTo(target.pos)
+            if distance < 850 and isSpellReady(_Q) then
+                castSpell(self.qSpellData, HK_Q, target)
+            end
+
+            if isSpellReady(_W) and distance < 850 and self.Menu.WHarrass:Value() then
+                local wObject = self:getWObject()
+                if wObject then
+                    self:castW(wObject, target)
+                end
+            end
+        end
+    end
+
+    function Syndra:AutoQ()
+        
+        if self.isInEQ or self.isInW then return end
+
+        local target = _G.SDK.TargetSelector:GetTarget(850, _G.SDK.DAMAGE_TYPE_MAGICAL);
+        
+        if target and target.activeSpell and target.activeSpell.valid and myHero.pos:DistanceTo(target.pos) < 805 then
+            castSpell(self.qSpellData, HK_Q, target)
+        end
+    end
+
+    function Syndra:LaneClear()
+        if HealthPrediction:ShouldWait() then return end
+        local bestQPosition, bestCount = self:getBestClearMinion()
+        
+        if bestQPosition and bestCount > 1 and isSpellReady(_Q) then
+            Control.CastSpell(HK_Q, bestQPosition)
+        end
+    end
+    
+    function Syndra:AntiMelee()
+        if not isSpellReady(_E) then return end
+
+        for i, enemy in pairs(getEnemyHeroes()) do
+            if enemy and not enemy.dead and enemy.valid and enemy.visible and myHero.pos:DistanceTo(enemy.pos) < self.Menu.AntiMelee:Value() + myHero.boundingRadius then
+                print("Anti-Melee")
+                self:castQE(enemy)
+            end
+        end
+    end
 
 ----------------------------------------------------
 -- Script starts here
